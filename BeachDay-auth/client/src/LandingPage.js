6e04e733121c051be7a0464699908a6716dec521
WilliamSmithWLS <wls00008@mix.wvu.edu>
2025-03-10 17:21:48 -0400

    Update LandingPage.js

--- BeachDay-auth/client/src/LandingPage.js
+++ BeachDay-auth/client/src/LandingPage.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+function LandingPage() {
+  return (
+    <div>
+      <h1>Welcome to Beach Day!</h1>
+      <p>Plan your perfect beach getaway.</p>
+      <Link to="/login">
+        <button>Login</button>
+      </Link>
+      <Link to="/register">
+        <button>Register</button>
+      </Link>
+    </div>
+  );
+}
+
+export default LandingPage;
